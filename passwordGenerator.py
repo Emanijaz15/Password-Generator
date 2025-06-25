1523de0444a7647823ce55419ffca6475805c937
Emanijaz15 <eijaz7357@gmail.com>
2025-06-25 21:36:32 +0500

    Update passwordGenerator.py

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -3,5 +3,7 @@ import string
 
 user=int(input("Enter length of your password: "))
 chars=string.ascii_letters + string.digits + string.punctuation
+password=""
 for i in range(user):
-    print(random.choice(chars),end="")
+    password+=random.choice(chars)
+print(password)
